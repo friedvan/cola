1f44f39918505853a9d592d0e7b40b0dddbde895
pzc <rabbitpzc@gmail.com>
2014-11-15 21:25:44 +0800

    add process watcher to restart stoped process and send a mail to notify

--- contrib/weibo/__init__.py
+++ contrib/weibo/__init__.py
@@ -20,10 +20,6 @@ Created on 2013-6-7
 @author: Chine
 '''
 
-import os
-import sys
-
-
 from cola.core.opener import MechanizeOpener
 from cola.core.urls import Url, UrlPatterns
 from cola.job import Job
@@ -33,6 +29,20 @@ from parsers import MicroBlogParser, ForwardCommentLikeParser,\
                     UserInfoParser, UserFriendParser
 from conf import starts, user_config, instances
 from bundle import WeiboUserBundle
+
+import sys
+import os
+import time
+import smtplib
+from email.mime.text import MIMEText
+import subprocess
+
+SLEEP_TIME=5
+
+mail_host="smtp.163.com"
+mail_user="rabbitlee_ok"
+mail_pass="564335491"
+mail_postfix="163.com"
 
 def login_hook(opener, **kw):
     username = str(kw['username'])
@@ -59,11 +69,46 @@ def get_job():
                instances=instances, debug=True, user_conf=user_config,
                login_hook=login_hook)
 
-
 def load_start(src):
     with open(src) as fin:
         return [str(line.strip()) for line in fin]
 
+
+def send_mail(to_list, sub, content):
+    me = mail_user+"<"+mail_user+"@"+mail_postfix+">"
+    msg = MIMEText(content)
+    msg['Subject'] = sub
+    msg['From'] = me
+    msg['To'] = ";".join(to_list)
+    try:
+        s = smtplib.SMTP()
+        s.connect(mail_host)
+        s.login(mail_user,mail_pass)
+        s.sendmail(me, to_list, msg.as_string())
+        s.close()
+        return True
+    except Exception, e:
+        print str(e)
+        return False
+
+
+def autohalt(filename):
+    while True:
+        ps_string = os.popen('ps -ax | grep '+filename+'|grep -v grep  ', 'r').read()
+        ps_strings = ps_string.strip().split('\n')
+        if len(ps_strings) < 2:
+            mailto_list = ["287535211@qq.com"]
+            send_mail(mailto_list, "process stop, restart ", str(time.time()))
+            cmds = ['python', '__init__.py']
+            subprocess.Popen(cmds)
+            # return
+        else:
+            print 'Still ' + len(ps_strings) + ' Processes, waiting %s min...' % SLEEP_TIME
+            time.sleep(60*SLEEP_TIME)
+
 if __name__ == "__main__":
     from cola.worker.loader import load_job
     load_job(os.path.dirname(os.path.abspath(__file__)))
+    autohalt()
+
+
