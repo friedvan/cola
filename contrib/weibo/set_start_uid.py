cc48dd104a4521febfdd15fd56f97101634e2023
pzc <rabbitpzc@gmail.com>
2014-11-26 22:00:56 +0800

    add start_uid script

---
+++ contrib/weibo/set_start_uid.py
@@ -0,0 +1,23 @@
+__author__ = 'pzc'
+
+
+import redis
+import sys
+
+REDIS_HOST = 'localhost'
+
+
+if '__name__' == '__main__':
+    if len(sys.argv) < 2:
+        print 'usage: python set_start_uid.py weibo_uid'
+        sys.exit(0)
+    rds = redis.Redis(host=REDIS_HOST)
+    start_uid = sys.argv[2]
+    try:
+        int(start_uid)
+        rds.rpush('cola:uids', start_uid)
+    except:
+        print 'usage: python set_start_uid.py weibo_uid'
+        sys.exit(0)
+
+    print 'success!'
